c564f6a8178009e5a826348d8005881bcfb601ee
Santi S <silvasant_@users.noreply.github.com>
2019-09-19 23:58:40 -0300

    removed rows with NA in latitude or longitude values

--- Data_preloading.R
+++ Data_preloading.R
@@ -4,6 +4,7 @@ library(readr)
 Acc <- read_csv("Project Data/Acc.csv", col_types = cols(Accident_Index = col_character(), 
       Date = col_date(format = "%d/%m/%Y"), 
       Time = col_time(format = "%H:%M")))
+Acc<-Acc[!is.na(Acc$Latitude)&!is.na(Acc$Longitude),]
 Cas <- read_csv("Project Data/Cas.csv", col_types = cols(Accident_Index = col_character()))
 Veh <- read_csv("Project Data/Veh.csv", col_types = cols(Accident_Index = col_character()))
 
