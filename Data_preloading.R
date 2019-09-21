539eb072ddda17b479db031b52793967bb987b0a
Santi S <silvasant_@users.noreply.github.com>
2019-09-21 17:39:32 -0300

    updated data_preloading with Veh_df which will be used for princing plots

--- Data_preloading.R
+++ Data_preloading.R
@@ -7,4 +7,10 @@ Acc <- read_csv("Project Data/Acc.csv", col_types = cols(Accident_Index = col_ch
 Acc<-Acc[!is.na(Acc$Latitude)&!is.na(Acc$Longitude),]
 Cas <- read_csv("Project Data/Cas.csv", col_types = cols(Accident_Index = col_character()))
 Veh <- read_csv("Project Data/Veh.csv", col_types = cols(Accident_Index = col_character()))
+Veh_df<-Veh[Veh$Vehicle_Reference==1,]
+Veh_df$Latitude<-as.numeric(Acc$Latitude[match(Veh_df$Accident_Index,Acc$Accident_Index)])
+Veh_df$Longitude<-as.numeric(Acc$Longitude[match(Veh_df$Accident_Index,Acc$Accident_Index)])
+Veh_df$Date<-Acc$Date[match(Veh_df$Accident_Index,Acc$Accident_Index)]
+Veh_df$District<-Acc$`Local_Authority_(District)`[match(Veh_df$Accident_Index,Acc$Accident_Index)]
+Veh_df$GeoCode<-Acc$LSOA_of_Accident_Location[match(Veh_df$Accident_Index,Acc$Accident_Index)]
 
